08d24e2749ce64e9328bf64ae37a3e23f0521c49
shiwx97 <77234613+shiwx97@users.noreply.github.com>
2023-05-19 22:00:26 +0800

    Add files via upload
    wavelet processing for signals

---
+++ preprocessing.py
@@ -0,0 +1,17 @@
+import pywt
+import numpy as np
+def denoise(data):
+    # 小波变换
+    coeffs = pywt.wavedec(data=data, wavelet='db5', level=9)
+    cA9, cD9, cD8, cD7, cD6, cD5, cD4, cD3, cD2, cD1 = coeffs
+
+    # 阈值去噪
+    threshold = (np.median(np.abs(cD1)) / 0.6745) * (np.sqrt(2 * np.log(len(cD1))))
+    cD1.fill(0)
+    cD2.fill(0)
+    for i in range(1, len(coeffs) - 2):
+        coeffs[i] = pywt.threshold(coeffs[i], threshold)
+
+    # 小波反变换,获取去噪后的信号
+    rdata = pywt.waverec(coeffs=coeffs, wavelet='db5')
+    return rdata
